53f8c82969370062a2724aa32a43ce0c7c050153
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-09-25 08:24:24 +0600

    updated

--- Bresenham line draw.cpp
+++ Bresenham line draw.cpp
@@ -1,53 +1,87 @@
-// 1) Line is drawn from left to right.
-// 2) x1 < x2 and y1 < y2
-// 3) Positive slope: Slope of the line is between 0 and 1.
-//    We draw a line from lower left to upper
 #include<bits/stdc++.h>
 #include<graphics.h>
-#include<dos.h>
+
 using namespace std;
 
-int main(){
-
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"c:\\tc\\bgi");
-setbkcolor(WHITE);
-
-float x,y,x0,y0,x1,y1,step,dx,dy,pk;
-int i;
-
-cout<<"Enter the value of starting point (x0,y0): ";
-cin>>x0>>y0;
-cout<<"Enter the value of ending point (x1,y1): ";
-cin>>x1>>y1;
-
-x=x0;
-y=y0;
-
-dx=abs(x1-x0);
-dy=abs(y1-y0);
-
-pk=2*dy-dx; //initial value of decision parameter pk
-
-
-cout<<"pk\t"<<"x\ty"<<endl;
-for(i=0;i<=dx;i++)
+void plotgraph(int screenWidth,int screenHeight)
 {
-    cout<<pk<<"\t"<<(int) x<<"\t"<<(int) y<<endl;
-    
-    putpixel(x,y,RED);
-    if(pk<0){
-        x=x+1;
-        y=y;
-        pk=pk+2*dy;
-    }
-    else{
-        x=x+1;
-        y=y+1;
-        pk=pk+2*dy-2*dx;
-    }
-
+	int i,j;
+	
+/*	for(i=screenWidth/2;i<=screenWidth;i=i+10)
+	{
+		for(j=screenHeight/2;j<=screenHeight;j=j+10)
+		{
+			setcolor(MAGENTA);
+			line(i,0,i,screenHeight);
+			line(0,j,screenWidth,j);
+		}
+	}
+		for(i=screenWidth/2;i>=0;i=i-10)
+	{
+		for(j=screenHeight/2;j>=0;j=j-10)
+		{
+			setcolor(MAGENTA);
+			line(i,0,i,screenHeight);
+			line(0,j,screenWidth,j);
+		}
+	}
+	*/
+	setcolor(YELLOW);
+	line(0,screenHeight/2,screenWidth,screenHeight/2);
+	line(screenWidth/2,0,screenWidth/2,screenHeight);
 }
-getch();
-closegraph();
+
+int main()
+{
+	int gd=DETECT,gm,i;
+	float xc,yc,r;
+	char c;
+	int lr=0,ud=0;
+	
+	int screenWidth=GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight=GetSystemMetrics(SM_CYSCREEN);
+	
+	initwindow(screenWidth,screenHeight,"window",2,2);
+	plotgraph(screenWidth,screenHeight);
+	
+//	cout<<"enter the radius of thre circle r:";
+//	cin>>r;
+//	cout<<"enter the center of thre circle (xc,yc):";
+//	cin>>xc>>yc;
+    int centerX=screenWidth/2;
+    int centerY=screenHeight/2;
+    r=40;
+    xc=50;
+    yc=50;
+    
+    setcolor(GREEN);
+    setfillstyle(SOLID_FILL,GREEN);
+	circle((centerX)+xc,centerY-yc,r);
+	floodfill(centerX+xc+1,centerY-yc+1,GREEN);
+	
+	while(1)
+	{
+		if(kbhit){
+		
+		c=getch();
+		cleardevice();
+		if(c=='a' || c==75)lr=lr-5;
+		if(c=='s' || c==80)ud=ud+5;
+		if(c=='d' || c==77)lr=lr+5;
+		if(c=='w' || c==72)ud=ud-5;
+		
+	//	cout<<xc+lr<<" "<<yc+ud<<endl;
+		setcolor(GREEN);
+        setfillstyle(SOLID_FILL,GREEN);
+	    circle((centerX)+xc+lr,centerY-yc+ud,r);
+	    floodfill(centerX+xc+1+lr,centerY-yc+1+ud,GREEN);
+	    plotgraph(screenWidth,screenHeight);
+	   // delay(10);
+	    
+	}
+		
+	}
+	
+	getch();
+	closegraph();
 }
